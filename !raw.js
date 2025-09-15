b19223edb9e7b688d9b07147ef9e234f39d6a118
denis <36066229+d1sv@users.noreply.github.com>
2025-09-15 14:24:01 +0300

    Update !raw.js

--- !raw.js
+++ !raw.js
@@ -5,34 +5,32 @@ https://github.com/Rafail1082GitHUB/rafail1982.ru
 https://loganettv.vercel.app/
 https://tvsatclub.info/viewtopic.php?t=41&start=1760
 
-
-мои (3)
-# https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/default.m3u8
-18+ есть, 3623 каналов
-https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/VseTV.m3u8
-https://dl.dropbox.com/scl/fi/0l59va8if6ly3t8pxb549/3.m3u8?rlkey=tuju0ak1qnq9tyscshlhkazq3&st=1m86etbo&dl=0
+мои тв (3)
+1) https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/default.m3u8
+VseTV, 3623 каналов (+18)
+2) https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/VseTV.m3u8
+путь) https://dl.dropbox.com/scl/fi/0l59va8if6ly3t8pxb549/3.m3u8?rlkey=tuju0ak1qnq9tyscshlhkazq3&st=1m86etbo&dl=0
 блочится goodstreem, удалить, найти аналог
-https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/GoodstreemTV.m3u8
+3) https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/GoodstreemTV.m3u8
 
-
-не мои, обновляемые (5)
-https://raw.githubusercontent.com/IPTVSHARED/iptv/refs/heads/main/IPTV_SHARED.m3u
-https://loganettv.vercel.app/all.m3u
-!!++ 49) EdemTV, 2828 каналов
-http://2906ada53139.mylistbest.net/playlists/uplist/efb9c4f04d9b67fadc2e974a6e0f9cba/playlist.m3u8
-!!++ 18+ 50) EdemRezervTV, блочится, 3250 каналов
-http://b30efcccd4de.iedem.com/playlists/uplist/3e6d2e82422e191340b44f9eba6339a5/playlist.m3u8
-++ 46) ProkazovTV, 3317 каналов
+обновляемые тв (5)
+0) https://raw.githubusercontent.com/IPTVSHARED/iptv/refs/heads/main/IPTV_SHARED.m3u
+0) https://loganettv.vercel.app/all.m3u
+46) ProkazovTV, 3317 каналов
 http://ournet.live/tv/igorprokazov.m3u
+49) EdemTV, 2828 каналов
+http://2906ada53139.mylistbest.net/playlists/uplist/efb9c4f04d9b67fadc2e974a6e0f9cba/playlist.m3u8
+50) EdemRezervTV, блочится, 3250 каналов  (+18)
+http://b30efcccd4de.iedem.com/playlists/uplist/3e6d2e82422e191340b44f9eba6339a5/playlist.m3u8
 
 кино (1)
+38) Contain Mediateka
 https://raw.githubusercontent.com/suzukantv/IPTV-FILM/refs/heads/main/mediateka.m3u
-++ кино много 38) Contain Mediateka
 
 найдены (1)
-есть 18, 3900 каналов
+1) 3900 каналов  (+18)
 http://6b6a16fcb5b1.faststreem.org/playlists/uplist/ed56f0a4000f89c682f89f86c523f183/playlist.m3u8
-есть 18, 3978 каналов
+2) 3978 каналов (+18)
 http://8077a3b70137.ottclub.net/playlists/uplist/f07f3c86fb04a2c3395fc84825335c50/playlist.m3u8
-есть 18, 3978 каналов
+3) 3978 каналов (+18)
 http://091317bca1db.zatikov.net/playlists/uplist/ec646e7a7e3959a7a3d87147be9036ee/playlist.m3u8
