48ad13289c0338b410b8bfd9471370d55f6f7210
denis <36066229+d1sv@users.noreply.github.com>
2025-09-15 14:28:20 +0300

    Update !raw.js

--- !raw.js
+++ !raw.js
@@ -1,21 +1,23 @@
-ссылки raw
-IPTV DRM Stream Player
+ссылки raw IPTV DRM Stream Player
 https://iptv.axenov.dev/
 https://github.com/Rafail1082GitHUB/rafail1982.ru
 https://loganettv.vercel.app/
 https://tvsatclub.info/viewtopic.php?t=41&start=1760
 
 мои тв (3)
-1) VseTV, 3623 каналов (+18)
+1) default
 https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/default.m3u8
-путь) https://dl.dropbox.com/scl/fi/0l59va8if6ly3t8pxb549/3.m3u8?rlkey=tuju0ak1qnq9tyscshlhkazq3&st=1m86etbo&dl=0
-2) https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/VseTV.m3u8
-3) блочится goodstreem, удалить, найти аналог
+2) VseTV, 3623 каналов (+18)
+https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/VseTV.m3u8
+https://dl.dropbox.com/scl/fi/0l59va8if6ly3t8pxb549/3.m3u8?rlkey=tuju0ak1qnq9tyscshlhkazq3&st=1m86etbo&dl=0
+3) блочится, удалить, найти аналог
 https://raw.githubusercontent.com/d1sv/ip/refs/heads/main/GoodstreemTV.m3u8
 
 обновляемые тв (5)
-0) https://raw.githubusercontent.com/IPTVSHARED/iptv/refs/heads/main/IPTV_SHARED.m3u
-0) https://loganettv.vercel.app/all.m3u
+0) IPTV_SHARED
+https://raw.githubusercontent.com/IPTVSHARED/iptv/refs/heads/main/IPTV_SHARED.m3u
+0) loganettv
+https://loganettv.vercel.app/all.m3u
 46) ProkazovTV, 3317 каналов
 http://ournet.live/tv/igorprokazov.m3u
 49) EdemTV, 2828 каналов
@@ -27,7 +29,7 @@ http://b30efcccd4de.iedem.com/playlists/uplist/3e6d2e82422e191340b44f9eba6339a5/
 38) Contain Mediateka
 https://raw.githubusercontent.com/suzukantv/IPTV-FILM/refs/heads/main/mediateka.m3u
 
-найдены (1)
+найдены (3) форум
 1) 3900 каналов  (+18)
 http://6b6a16fcb5b1.faststreem.org/playlists/uplist/ed56f0a4000f89c682f89f86c523f183/playlist.m3u8
 2) 3978 каналов (+18)
